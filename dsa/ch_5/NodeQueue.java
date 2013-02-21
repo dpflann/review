2377db2d49ec4a827ae9412dd5a3a7db175c9211
Daniel Flannery <d.p.flannery@gmail.com>
2013-02-21 16:38:41 -0500

    Small refactoring on NodeQueue, and no more white space

--- dsa/ch_5/NodeQueue.java
+++ dsa/ch_5/NodeQueue.java
@@ -26,12 +26,11 @@ public class NodeQueue<E> {
     Node<E> node = new Node<E>(element, null);
     if (isEmpty()) {
       front = node;
-      back = node;
     }
     else {
       back.setNext(node);
-      back = node;
     }
+    back = node;
     size++;
   }
 
@@ -41,24 +40,22 @@ public class NodeQueue<E> {
       return null;
     }
     else {
+      Node<E> node = front;
       if (size > 1) {
-        Node<E> node = front;
         front = node.getNext();
-        size--;
-        return node.getElement();
       }
       else {
-        Node<E> node = front;
         front = null;
         back = null;
-        size--;
-        return node.getElement();
       }
+      node.setNext(null);
+      size--;
+      return node.getElement();
     }
   }
 
   public E getFront() {
-    if (isEmpty()) { 
+    if (isEmpty()) {
       System.out.println("Queue is empty. Returning null");
       return null;
     }
@@ -104,6 +101,6 @@ public class NodeQueue<E> {
     System.out.println(queue.dequeue() + " dequeued");
     System.out.println(queue.dequeue() + " dequeued");
     System.out.println(queue.dequeue() + " dequeued");
-    
+
   }
 }//end Queue
