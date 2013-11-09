70be9ebb853d7d51a65289976f6d84ac3a9f2777
Daniel Flannery <d.p.flannery@gmail.com>
2013-11-09 13:54:33 -0500

    Alternate approach to Sieve of Eratosthenes.

--- epi/sieve_of_eratosthenes.py
+++ epi/sieve_of_eratosthenes.py
@@ -15,10 +15,43 @@ def sieve_of_eratosthenes(n):
     i += 1
   return numbers
 
+def alternate_sieve(n):
+  numbers = []
+  for i in range(2, n + 1):
+    j = 0
+    add = True
+    while (j < len(numbers)):
+      if i % numbers[j] == 0:
+        add = False
+        break
+      j += 1
+    if (add):
+      numbers.append(i)
+  return numbers
 
+
+#### TESTING
 print("n = 10")
 print(sieve_of_eratosthenes(10))
 
+def test_sieve():
+  assert(sieve_of_eratosthenes(2) == [2])
+  assert(sieve_of_eratosthenes(3) == [2,3])
+  assert(sieve_of_eratosthenes(10) == [2,3,5,7])
+  print("Tests pass")
+
+def test_alternate_sieve():
+  assert(alternate_sieve(2) == [2])
+  assert(alternate_sieve(3) == [2,3])
+  assert(alternate_sieve(10) == [2,3,5,7])
+  print("Tests pass")
+
+print("Testing Sieve of Eratosthenes")
+test_sieve()
+print("Testing Alternate Sieve")
+test_alternate_sieve()
+
+### If you want to use commandline arguments for the sieve
 if (len(sys.argv) == 2):
   try:
     n = int(sys.argv[1])
