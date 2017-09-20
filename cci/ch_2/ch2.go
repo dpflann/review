9f8e5c5172657b212bbc568f90f1fcba6b51f993
dpflann <d.p.flannery@gmail.com>
2017-09-19 20:31:36 -0400

    Ch.2: problem 3 in go

--- cci/ch_2/ch2.go
+++ cci/ch_2/ch2.go
@@ -106,6 +106,17 @@ func (LL *LinkedList) String() string {
 	return strLL
 }
 
+func DeleteNode(n *Node) {
+	if n == nil {
+		return
+	}
+	if n.Next != nil {
+		tmpNode := n.Next
+		n.Next = tmpNode.Next
+		n.Data = tmpNode.Data
+	}
+}
+
 func main() {
 	fmt.Println("====== 2.1 ======")
 	list1, dupeList1, expectedList1 := &LinkedList{}, &LinkedList{}, &LinkedList{}
@@ -140,38 +151,25 @@ func main() {
 	fmt.Println(RemoveDuplicates2(dupeList1).Equals(expectedList1) == true)
 	fmt.Println("=================")
 
-	/*
 	fmt.Println("====== 2.3 ======")
-		list1, dupeList1, expectedList1 := &LinkedList{}, &LinkedList{}, &LinkedList{}
-		for i := 0; i < 10; i++ {
-			n1 := &Node{nil, i}
-			n2 := &Node{nil, i}
-			n3 := &Node{nil, i}
-			n4 := &Node{nil, i}
-			list1.AddNode(n1)
-			expectedList1.AddNode(n2)
-			dupeList1.AddNode(n3)
-			dupeList1.AddNode(n4)
-		}
+	list2, expectedList2 := &LinkedList{}, &LinkedList{}
+	n1 := &Node{nil, 11}
+	for i := 0; i < 10; i++ {
+		n1 := &Node{nil, i}
+		n2 := &Node{nil, i}
+		list2.AddNode(n1)
+		expectedList2.AddNode(n2)
+	}
+	list2.AddNode(n1)
+	for i := 12; i < 15; i++ {
+		n1 := &Node{nil, i}
+		n2 := &Node{nil, i}
+		list2.AddNode(n1)
+		expectedList2.AddNode(n2)
+	}
 
-		fmt.Println("RemoveDuplicates1(...)")
-		fmt.Println(RemoveDuplicates1(list1).Equals(expectedList1) == true)
-		fmt.Println(RemoveDuplicates1(dupeList1).Equals(expectedList1) == true)
-
-		list1, dupeList1, expectedList1 = &LinkedList{}, &LinkedList{}, &LinkedList{}
-		for i := 0; i < 10; i++ {
-			n1 := &Node{nil, i}
-			n2 := &Node{nil, i}
-			n3 := &Node{nil, i}
-			n4 := &Node{nil, i}
-			list1.AddNode(n1)
-			expectedList1.AddNode(n2)
-			dupeList1.AddNode(n3)
-			dupeList1.AddNode(n4)
-		}
-		fmt.Println("RemoveDuplicates2(...)")
-		fmt.Println(RemoveDuplicates2(list1).Equals(expectedList1) == true)
-		fmt.Println(RemoveDuplicates2(dupeList1).Equals(expectedList1) == true)
-	*/
+	fmt.Println("DeleteNode(...)")
+	DeleteNode(n1)
+	fmt.Println(list2.Equals(expectedList2) == true)
 	fmt.Println("=================")
 }
