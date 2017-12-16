cec69dcf04791554712f2014e6dfc404ef33f2b2
dpflann <d.p.flannery@gmail.com>
2017-12-16 17:09:16 -0500

    Begin 5.1: add more bit clearing functionality

--- cci/ch_5/ch5.go
+++ cci/ch_5/ch5.go
@@ -33,11 +33,31 @@ func clearBit(num, i uint) uint {
 	return num & ^(1 << i)
 }
 
+func clearBitsItoZero(num, i uint) uint {
+	return num & ^((1 << (i + 1)) - 1)
+}
+
+func clearBitsMSBtoI(num, i uint) uint {
+	return num & ((1 << i) - 1)
+}
+
+//\\//\\ 5.1 //\\//\\
+// Assumes M will be fit within N between j and i.
+// j and i have at least enough space for M.
+func Insert(N, M, i, j uint) uint {
+	// Find the section N between j and i
+	// clear this section
+	// or this section with M shift by (j - i)
+	return 1
+}
+
 //\\//\\//\\ MAIN //\\//\\//\\
 func main() {
 	fmt.Println(problemTitle("5.1"))
 	fmt.Println(getBit(2, 1) == 1)
 	fmt.Println(setBit(5, 1) == 7)
 	fmt.Println(clearBit(7, 1) == 5)
+	fmt.Println(clearBitsItoZero(15, 2) == 8)
+	fmt.Println(clearBitsMSBtoI(15, 2) == 3)
 	fmt.Println(problemEndline(problemTitle("5.1")))
 }
